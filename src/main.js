85b4f5f6dd6c6fd6983be89341ef08d8ab33ffb8
DaisukeMatsuura <mail@brightful.jp>
2020-12-26 11:07:08 +0900

    AxiosのGET時のヘッダーにAccept-Languageを追加

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'https://accounts.spotify.com'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.headers.get['Accept-Language'] = 'ja;q=1'
 
 new Vue({
   render: h => h(App),
