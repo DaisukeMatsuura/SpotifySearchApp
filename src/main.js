4db8b5043527c7fd6540f4d1df01e9087dad5a06
DaisukeMatsuura <mail@brightful.jp>
2020-12-26 14:06:09 +0900

    install TailwindCSS & DestyleCSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import './assets/tailwind.css'
+import 'destyle.css'
 
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'https://accounts.spotify.com'
